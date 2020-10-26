2952c18d8a48c75767df25d8767138b1506aada6
Shakil Hossain <shakilhossain38@gmail.com>
2020-10-27 01:08:24 +0600

    First Commit

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="";
+$db_name="flutter_food_app";
+$conn=mysqli_connect($host,$user, $password,$db_name);
+if($conn){
+	echo "Not Connected";
+}
+
+?>
---
+++ foods/getfoods.php
@@ -0,0 +1,16 @@
+<?php
+
+include '../config/config.php';
+
+$result=array();
+$queryFood= "SELECT * FROM foods";
+$queryResult= mysqli_query($conn, $queryFood);
+while ($row=mysqli_fetch_assoc($queryResult)) {
+	//$result=$row;
+	array_push($result,$row);
+}
+
+
+echo json_encode($result);
+
+?>
